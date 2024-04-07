3dfe125f0ee7ea04ffd97d9cc39c7dd880457e8c
appledefender <appledefender@gmail.com>
2024-04-08 01:38:31 +0500

    cahnge

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 export default function Loader() {
   return (
-    <div className="flex  justify-center items-center h-[100vh] bg-black">
+    <div className="flex  justify-center items-center h-[100vh] fixed top-0 left-0 z-[9999999] w-full bg-black">
       <div class="lds-ellipsis">
         <div></div>
         <div></div>
