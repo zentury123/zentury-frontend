924648a0bb9e62c8e9aae01184fb25b4d603188f
appledefender <appledefender@gmail.com>
2024-04-09 01:47:21 +0500

    cahnges

--- src/app/direct-sale/[slug]/page.js
+++ src/app/direct-sale/[slug]/page.js
@@ -28,7 +28,7 @@ const RealState = async ({ params }) => {
     return d;
   };
   const data = await fetchData();
-
+console.log(data?.document1?.fileName)
   return (
     <div className="bg-white min-h-[100vh]">
       <div className="lg:mt-[190px] mt-[90px] text-black xl:px-[81px] px-[24px]">
@@ -44,9 +44,9 @@ const RealState = async ({ params }) => {
           <div className="xl:col-span-4 lg:col-span-5 md:col-span-6 col-span-12  ">
             <ImageGallery galleryImages={data?.images || []} />
             <Map location={data?.coordinates} zoom={data?.zoom} />
-            {data?.document1 || data?.document2 && <div><p class="text-[21px] font-semibold mt-[58px]">Dokumenty na stiahnutie</p>
-              {data?.document1 && <a href={data?.document1} download={true} class="text-[#44525E] text-[12px] block mt-[20px]">Dokument 1.pdf</a>}
-              {data?.document2 && <a href={data?.document2} download={true} class="text-[#44525E] text-[12px] block">Dokument 2.pdf</a>}
+            {(data?.document1?.data || data?.document2?.data) && <div><p class="text-[21px] font-semibold mt-[58px]">Dokumenty na stiahnutie</p>
+              {data?.document1?.data && <a href={data?.document1?.data} download={true} class="text-[#44525E] text-[12px] block mt-[20px]">{data?.document1?.fileName}</a>}
+              {data?.document2?.data && <a href={data?.document2?.data} download={true} class="text-[#44525E] text-[12px] block">{data?.document2?.fileName}</a>}
             </div>}
           </div>
           <div className="xl:col-span-8 lg:col-span-7 md:col-span-6 col-span-12 text-[12px] text-[#44525E] leading-[18px]">
